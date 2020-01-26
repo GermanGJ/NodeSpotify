748ff812347c0f21e58bf4470b694480a272e857
Server2GJ <GermanGomezJimenez@gmail.com>
2020-01-26 04:14:48 -0500

    2GJ Id31 Imagenes Artista. OK Terminada capa Artista.

--- controllers/artist.js
+++ controllers/artist.js
@@ -140,10 +140,93 @@ function deleteArtist(req, res){
 }
 
 
+
+function uploadImage(req, res){
+    var userId = req.params.id;
+    var file_name = 'No subido...';
+
+    if(req.files){
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        console.log(file_split);
+        console.log(file_ext);
+
+        if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif'){
+            User.findByIdAndUpdate(userId, {image: file_name}, (err, userUpdated) => { 
+                if(err){
+                    res.status(500).send({message: 'Error al actualizar el usuario.'});
+                }else{
+                    if(!userUpdated){
+                        res.status(404).send({message: 'No se ha podido actualizar el usuario.'});
+                    }else{
+                        res.status(200).send({user: userUpdated});
+                    }
+                }
+            });
+        }
+    }else{
+        res.status(200).send({message: 'No has subido ninguna imagen...'});
+    }
+}
+
+
+function uploadImage(req, res){
+    var artistId = req.params.id;
+    var file_name = 'No subido---';
+
+    if(req.files){
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        console.log(file_split);
+        console.log(file_ext);
+
+        if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif'){
+            Artist.findByIdAndUpdate(artistId, {image: file_name}, (err, artistUpdated) => { 
+                if(err){
+                    res.status(500).send({message: 'Error al actualizar el artista.'});
+                }else{
+                    if(!artistUpdated){
+                        res.status(404).send({message: 'No se ha podido actualizar el artista.'});
+                    }else{
+                        res.status(200).send({user: artistUpdated});
+                    }
+                }
+            });
+        }
+    }else{
+        res.status(200).send({message: 'No has subido ninguna imagen...'});
+    }
+}
+
+function getImageFile(req, res){
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/artists/' + imageFile;
+
+    fs.exists(path_file, function(exists){
+        if(exists){
+            res.sendFile(path.resolve(path_file));    
+        }else{
+            res.status(200).send({message: 'No existe la imagen...'});
+        }
+    });
+}
+
+
+
 module.exports = {
     getArtist,
     getArtists,
     saveArtist,
     updateArtist,
-    deleteArtist
+    deleteArtist,
+    uploadImage,
+    getImageFile
 };
