8977d188acc7ccb8aa4f54f3c8862c4ba985463d
2GJ <GermanGJ@Hotamil.com>
2020-02-04 21:57:44 -0500

    2GJ Id:48 Directorios de la aplicacion.

--- api/controllers/song.js
+++ api/controllers/song.js
@@ -118,10 +118,59 @@ function deleteSong(req, res){
     });
 }
 
+
+function uploadFile(req, res){
+    var songId = req.params.id;
+    var file_name = 'No subido...';
+
+    if(req.files){
+        var file_path = req.files.file.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        console.log(file_split);
+        console.log(file_ext);
+
+        if (file_ext == 'mp3' || file_ext == 'ogg'){
+            Song.findByIdAndUpdate(songId, {file: file_name}, (err, songUpdate) => { 
+                if(err){
+                    res.status(500).send({message: 'Error al actualizar el usuario.'});
+                }else{
+                    if(!songUpdate){
+                        res.status(404).send({message: 'No se ha podido actualizar el usuario.'});
+                    }else{
+                        res.status(200).send({song: songUpdate});
+                    }
+                }
+            });
+        }
+    }else{
+        res.status(200).send({message: 'No has subido ninguna imagen...'});
+    }
+}
+
+
+function getSongFile(req, res){
+    var imageFile = req.params.songFile;
+    var path_file = './uploads/songs/' + imageFile;
+
+    fs.exists(path_file, function(exists){
+        if(exists){
+            res.sendFile(path.resolve(path_file));    
+        }else{
+            res.status(200).send({message: 'No existe la imagen...'});
+        }
+    });
+}
+
 module.exports = {
     getSong,
     saveSong,
     getSongs,
     updateSong,
-    deleteSong
+    deleteSong,
+    uploadFile,
+    getSongFile
 };
